3f182e22ff21f6eba5e9e82447b70fa9d61baaac
tadashi-aikawa <syou.maman@gmail.com>
2023-07-16 00:33:30 +0900

    Remove an unnecessary command

--- src/commands.ts
+++ src/commands.ts
@@ -6,18 +6,5 @@ export function createCommands(
   appHelper: AppHelper,
   settings: Settings
 ): Command[] {
-  return [
-    {
-      id: "main-command",
-      name: "Main command",
-      checkCallback: (checking: boolean) => {
-        if (appHelper.getActiveFile() && appHelper.getActiveMarkdownView()) {
-          if (!checking) {
-            // TODO:
-          }
-          return true;
-        }
-      },
-    },
-  ];
+  return [];
 }
