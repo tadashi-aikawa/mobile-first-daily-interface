ee18b03396699eaff80bf20b9f6405f0b7c0ed85
tadashi-aikawa <syou.maman@gmail.com>
2023-10-08 20:25:05 +0900

    ESLintのテストケース修正

--- src/utils/meta-helper.test.ts
+++ src/utils/meta-helper.test.ts
@@ -25,7 +25,7 @@ describe("getCoverUrl", () => {
   test.concurrent.each`
     name          | url                                                                                     | expected
     ${"mockito"}  | ${"https://site.mockito.org/"}                                                          | ${undefined}
-    ${"ESLint"}   | ${"https://eslint.org/docs/latest/rules"}                                               | ${"https://eslint.org/icon-512.png"}
+    ${"ESLint"}   | ${"https://eslint.org/docs/latest/rules"}                                               | ${"https://eslint.org/og?title=Rules%20Reference&summary=A%20pluggable%20and%20configurable%20linter%20tool%20for%20identifying%20and%20reporting%20on%20patterns%20in%20JavaScript.%20Maintain%20your%20code%20quality%20with%20ease.%0A&is_rule=false&recommended=&fixable=&suggestions="}
     ${"voicy"}    | ${"https://voicy.jp/channel/1380/459280"}                                               | ${"https://ogp-image.voicy.jp/ogp-image/story/0/1380/459280"}
     ${"Cargo"}    | ${"https://doc.rust-lang.org/cargo/reference/publishing.html"}                          | ${undefined}
     ${"GIGAZINE"} | ${"https://gigazine.net/news/20230322-windows-11-snipping-tool-vulnerability/"}         | ${"https://i.gzn.jp/img/2023/03/22/windows-11-snipping-tool-vulnerability/00_m.jpg"}
